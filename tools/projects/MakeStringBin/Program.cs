a61fa5f714194612d743479113d54f7598ebe41e
Rick Gibbed <rick@gibbed.us>
2020-06-19 01:42:11 -0500

    Add MakeStringBin project.

---
+++ tools/projects/MakeStringBin/Program.cs
@@ -0,0 +1,79 @@
+﻿/* Copyright (c) 2020 Rick (rick 'at' gibbed 'dot' us)
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NDesk.Options;
+
+namespace MakeStringBin
+{
+    internal class Program
+    {
+        private static string GetExecutableName()
+        {
+            return Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        }
+
+        private static void Main(string[] args)
+        {
+            bool showHelp = false;
+
+            var options = new OptionSet()
+            {
+                { "h|help", "show this message and exit", v => showHelp = v != null },
+            };
+
+            List<string> extras;
+
+            try
+            {
+                extras = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("{0}: ", GetExecutableName());
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `{0} --help' for more information.", GetExecutableName());
+                return;
+            }
+
+            if (extras.Count < 1 || extras.Count > 2 || showHelp == true)
+            {
+                Console.WriteLine("Usage: {0} [OPTIONS]+ input_txt [output_u16bin]", GetExecutableName());
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            var inputPath = extras[0];
+            var outputPath = extras.Count > 1 ? extras[1] : Path.ChangeExtension(inputPath, ".u16bin");
+
+            var text = File.ReadAllText(inputPath);
+            var bytes = Encoding.BigEndianUnicode.GetBytes(text);
+            Array.Resize(ref bytes, bytes.Length + 2); // add a null
+            File.WriteAllBytes(outputPath, bytes);
+        }
+    }
+}
